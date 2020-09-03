8272d7b82447d3f7bfee9a346e101e6c4c3672a4
Matthew <matthedm@uci.edu>
2020-09-02 19:00:14 -0700

    Initial Commit

---
+++ 09-life-cycle-of-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  09-life-cycle-of-project
+//
+//  Created by Matt on 9/2/20.
+//  Copyright © 2020 Matt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
